275afd979816ef29246a79a4824d6339e6324ac8
Peter Pojtek <peter.pojtek@ceracare.co.uk>
2022-08-22 17:44:33 +0200

    WCF Services added

---
+++ PacketaConnector/Services/SpsCarrier.cs
@@ -0,0 +1,30 @@
+﻿
+
+using PacketaConnector.DTO.GetPacketStatus;
+using PacketaConnector.Interfaces;
+using Webship;
+using Webtrace;
+
+namespace PacketaConnector.Services
+{
+    public class SpsCarrier : ICarrier
+    {
+        private readonly WebshipWebServiceClient webshipClient;
+        private readonly WebtracePortTypeClient webtraceClient;
+        
+        public Task<string> CreatePackage(Packet packet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetLabel(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetPacketStatusResponse.response> GetPackageInfo(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
